c368fb3a0ebc567b61151dfeb8113285b1ced535
pulamc <karpol1245@gmail.com>
2019-10-28 14:31:18 +0100

    Added production build

---
+++ build/precache-manifest.bc6d80e0529e70297ce1cf34b36b135f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a31edb543fa021c16a69a5d48f2336",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37d646bc576420a9e42c",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6c55b565125352e2ca0c",
+    "url": "./static/js/2.80450006.chunk.js"
+  },
+  {
+    "revision": "37d646bc576420a9e42c",
+    "url": "./static/js/main.51203f4e.chunk.js"
+  },
+  {
+    "revision": "f0a5cab12031f8f4d19b",
+    "url": "./static/js/runtime~main.24edbdb3.js"
+  }
+]);
